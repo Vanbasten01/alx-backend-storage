ec6f29f131f4b26417cf2d2806a32640ca0b392c
Vanbasten01 <fouad.yasin01@gmail.com>
2024-01-23 05:03:21 -0800

    List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" a Python function that lists all documents in a collection """
+
+def list_all(mongo_collection):
+    """ a Python function that lists all documents in a collection """
+    return mongo_collection.find()
