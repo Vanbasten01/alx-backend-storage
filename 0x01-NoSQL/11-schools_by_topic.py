c84fc09d950de18a9e95ce8494a734142c989267
Vanbasten01 <fouad.yasin01@gmail.com>
2024-01-23 06:01:11 -0800

    Where can I learn Python?

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a Python function that returns the list of school
+    having a specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """  a Python function that returns the list
+    of school having a specific topic """
+    return mongo_collection.find({"topics": topic})
